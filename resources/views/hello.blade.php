b95da6073c022e60e5abe7669fcedf43c0b85977
kokobells <kokobellsprintingpress@gmail.com>
2024-02-01 16:57:25 +0800

    GI

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Docuzxcments</title>
+    <title>Docuzxsdfsdfsdfcments</title>
 </head>
 <body>
+ <p>HELLO</p>
     
 </body>
 </html>
