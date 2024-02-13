a4b64a01c32d75e26d4177c609ac6345f3eef117
kokobells <kokobellsprintingpress@gmail.com>
2024-02-13 10:15:20 +0800

    02-13-2024

--- database/migrations/2024_02_12_080349_create_student_table.php
+++ database/migrations/2024_02_12_080349_create_student_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string("FirstName");
+            $table->string("LastName");
+            $table->string("Section");
             $table->timestamps();
         });
     }
