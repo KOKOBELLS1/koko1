918e8661507b2952df5af409893fbf2bdbf56935
kokobells <kokobellsprintingpress@gmail.com>
2024-02-14 17:19:27 +0800

    Save me

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     //
+    public function register(Request $request)
+    {
+        $data= $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|unique:users,email',
+            'password'=>'required|string|confirmed'
+            
+        ]);
+        $user= User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>bcrypt($data['password'])
+        ]);
+        $token=$user->createToken('sanctum-Token')->plainTextToken;
+        $response = [
+            'user'=>$user,
+            'token'=>$token
+        ];
+        return response($response, 201);
+    }
+    public function login(Request $request)
+    {
+        $data= $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string'
+            
+        ]);
+        $user= User::where('email',$data['email'])->first();
+        if(!$user || !Hash::check($data['password'] , $user->password))
+        {
+            return response([
+                "message"=>"Bad Credential"
+            ],401);
+        }
+
+
+        $token=$user->createToken('sanctum-Token')->plainTextToken;
+        $response = [
+            'user'=>$user,
+            'token'=>$token
+        ];
+        return response($response, 201);
+    }
+
+
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            "message"=>"Logout Succesfully"
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,13 +15,22 @@ use App\Http\Controllers\StudentController;
 |
 */
 Route::get('/student',[StudentController::class,'index']);
-Route::post('/student',[StudentController::class,'store']);
 Route::get('/student/{id}',[StudentController::class,'show']);
-Route::put('/student/{id}',[StudentController::class,'update']);
-Route::delete('/student/{id}',[StudentController::class,'destroy']);
 Route::get('/student/search/{name}',[StudentController::class,'search']);
 
+Route::POST('/register',[AuthController::class,'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::POST('/login',[AuthController::class,'login']);
+
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::post('/student',[StudentController::class,'store']);
+    Route::delete('/student/{id}',[StudentController::class,'destroy']);
+    Route::put('/student/{id}',[StudentController::class,'update']);
+
+    Route::POST('/logout',[AuthController::class,'logout']);
 });
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
